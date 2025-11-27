c279917ca9b3ff6ff7504852476613dd5141e1ee
Soumya <soumyadiphaldar3@gmail.com>
2025-11-27 23:32:59 +0530

    implement socket.io

--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const socketIo = require("socket.io");
+const http = require("http");
 const cookieParser = require("cookie-parser");
+const chatSocket = requrie("./src/socket/chatSocket.js");
 const connectDb = require("./src/config/db");
 const authRouter = require("./src/routes/user.route.js");
 const chatRouter = require("./src/routes/chat.route.js");
 const messageRouter = require("./src/routes/message.route.js");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: { origin: "*" }
+});
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(cookieParser());
+
+chatSocket(io);
 
 app.use("/user", authRouter);
 app.use("/chat", chatRouter);
@@ -20,7 +29,7 @@ app.use("/message", messageRouter);
 
 connectDb()
 .then(() => {
-    app.listen(port, () => {
+    server.listen(port, () => {
         console.log(`Server is listening at http://localhost${port}`);
     })
 })
---
+++ server/src/socket/chatSocket.js
@@ -0,0 +1,58 @@
+const jwt = require("jsonwebtoken");
+const apiError = require("../utils/apiError.js");
+
+function checkSocket(io) {
+    io.on("connection", (socket) => {
+        console.log("socket connected: ", socket.id);
+    })
+
+    //Authenticate socket user
+    try {
+        const token = socket.handshake.auth?.token;
+
+        if (!token) {
+           console.log("No token");
+           return socket.disconnect();
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        socket.user = decodedToken;
+
+        console.log("User authenticated..", decodedToken._id);
+    } catch (error) {
+        throw new apiError(400, "Invalid token", error.message);
+        return socket.disconnect();
+    }
+
+    socket.join(socket.user._id);
+
+    socket.on("join_chat", (chatId) => {
+        socket.join(chatId);
+        console.log(`User ${socket.user._id} joined chat ${chatId}`);
+    })
+
+    socket.on("typing", (chatId) => {
+        socket.to(chatId).emit("typing", socket.user._id);
+    });
+    socket.on("stop_typing", (chatId) => {
+        socket.to(chatId).emit("stop_typing", socket.user._id);
+    });
+
+    socket.on("new_message", (message) => {
+        const chat = message.chat;
+
+        if (!chat || !chat.users) return;
+
+        chat.user.foEach((userId) => {
+            if (userId.toString() !== socket.user._id.toString()) {
+                socket.to(userId).emit("message received", message);
+            }
+        })
+    });
+
+    socket.on("disconnect", () => {
+        console.log(`User disconnected ${socket.user._id}`)
+    });
+};
+
+module.exports = checkSocket;
